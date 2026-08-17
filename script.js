8d1b2f7d0cd0bad22e1a1e632498a4601131d891
anonymous <anupriya.dutt21@gmail.com>
2026-08-17 22:29:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
-let player = null;
-let youtubeReady = false;
+let player;
+let musicPlaying = false;
+
+const YOUTUBE_VIDEO_ID = "32YzafO9Bmo";
 
 function onYouTubeIframeAPIReady() {
+    player = new YT.Player("youtubePlayer", {
+        height: "0",
+        width: "0",
 
-    player = new YT.Player("youtube-player", {
-
-        height: "1",
-        width: "1",
-
-        videoId: "32YzafO9Bmo",
+        videoId: YOUTUBE_VIDEO_ID,
 
         playerVars: {
             autoplay: 0,
             controls: 0,
             loop: 1,
-            playlist: "32YzafO9Bmo",
-            playsinline: 1,
-            rel: 0
+            playlist: YOUTUBE_VIDEO_ID
         },
 
         events: {
             onReady: function () {
-                youtubeReady = true;
-                console.log("Music player ready");
+                console.log("YouTube music player is ready ❤️");
             }
         }
-
     });
 }
 
 
-function openLetter() {
+document.getElementById("musicButton").addEventListener("click", function () {
 
-    if (youtubeReady && player) {
-        player.unMute();
-        player.setVolume(100);
-        player.playVideo();
+    if (!player) {
+        alert("Please wait a few seconds and try again ❤️");
+        return;
     }
+
+    if (musicPlaying) {
+
+        player.pauseVideo();
+
+        musicPlaying = false;
+
+        this.innerHTML = "🎵 Play Birthday Song";
+
+    } else {
+
+        player.playVideo();
+
+        musicPlaying = true;
+
+        this.innerHTML = "🔇 Pause Birthday Song";
+    }
+
+});
+
+
+function openLetter() {
 
     const letter = document.getElementById("letter");
 
